6fef80b220697333cad380ad0081029cb237debb
Amarah Mir <amarahmir638@gmail.com>
2025-09-17 20:07:34 +0530

    First commit - SmartStudy setup

---
+++ StudyPortal/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class StudyportalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'StudyPortal'
+from django.apps import AppConfig
+
