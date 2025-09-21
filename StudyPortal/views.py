dc34a5f0ec7a7b1164fa17235648b1e7f4aeedcd
Amarah Mir <amarahmir638@gmail.com>
2025-09-21 15:42:16 +0530

    Feature2: added About page with templates, vie, and URL

--- StudyPortal/views.py
+++ StudyPortal/views.py
@@ -1,3 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 def hello_feature1(request):
     return HttpResponse("Hello from Feature 1 branch")
+
+def about_page(request):
+    return render(request, 'StudyPortal/about.html')
